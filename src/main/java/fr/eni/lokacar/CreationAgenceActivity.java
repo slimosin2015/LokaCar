48bb5b1dda3ea68e01842058fb5cc8bb4ca99277
slimosin2015 <sebastien.limosin2015@campus-eni.fr>
2016-05-02 13:50:26 +0200

    test1

---
+++ src/main/java/fr/eni/lokacar/CreationAgenceActivity.java
@@ -0,0 +1,16 @@
+package fr.eni.lokacar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CreationAgenceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creation_agence);
+
+
+        //test
+    }
+}
